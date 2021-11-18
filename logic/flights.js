1242c2aa48bc366d4d780f679d8e93bc29261e12
Peter Lin <plin@petroiq.com>
2021-11-17 14:57:12 -1000

    task 1 & 2

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,16 @@
 function Flights() {
-
+    var calculateNumberOfFlights = (passengers, capacity) =>{
+        var flights 
+        if(passengers < 0 || (!Number.isInteger(Number(passengers)))) throw new Error("The number of passengers must be a positive integer value")
+        if(capacity <0 || (!Number.isInteger(Number(capacity)))) throw new Error("The capacity of the flight must be a positive integer value")
+        if(passengers % capacity == 0) {
+            flights = passengers/capacity;
+        } else {
+            flights = Math.floor(passengers/capacity + 1)
+        }
+        return flights
+    }
+    return {calculateNumberOfFlights}
 }
 
+module.exports = Flights()
