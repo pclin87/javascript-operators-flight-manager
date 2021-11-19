82547883bb2ab2b25b541ee97848cb38c77f4d72
Peter Lin <plin@petroiq.com>
2021-11-19 13:07:06 -1000

    tast 4

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,11 @@
 function Passengers() {
-
+    checkFlightCapacity = (capacity, passengersNumbers) => {
+        passengersNumbers = 0
+        passengersNumbers.forEach((number) => passengersNumbers += number)
+        if (passengersNumbers < capacity) {
+            return passengersNumbers
+        } else throw new Error("The number of oassengers of the flight must be less than the capacity of the flight")
+    }
+    return checkFlightCapacity
 }
-
+module.exports = Passengers();
