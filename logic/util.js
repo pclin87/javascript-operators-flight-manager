af615f078540a1844ed79a46ed4f16e236054ecd
Peter Lin <plin@petroiq.com>
2021-11-22 12:42:34 -1000

    6

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,10 @@
 function Util() {
-
+    calculateTotalDistributedPassengers = (distributedPassengers) => {
+        let totalDistributedPassengers = 0
+        distributedPassengers.forEach((o) => totalDistributedPassengers += Object.values(o))
+        return totalDistributedPassengers
+    }
+    return {calculateTotalDistributedPassengers}
 }
 
+module.exports = Util();
