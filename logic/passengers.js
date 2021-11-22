49c7b994476b698840e8386731f99edee8fe1acb
Peter Lin <plin@petroiq.com>
2021-11-22 12:11:29 -1000

    5

--- logic/passengers.js
+++ logic/passengers.js
@@ -6,61 +6,58 @@ function Passengers() {
             throw new Error("Flight capacity (" + capacity + ") exceeded. You have " + passengersNumber + " passengers.");
         } return passengersNumber;
     }
-    distributeAllSeatsToAllPassengers = (vip, regular, numberFlights, businessSeats, economySeats) =>{
+    function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers, nrOfFlights, 
+        businessSeatsPerFlight, economySeatsPerFlight) {
 
-        let vipPassengersWithBusinessSeats = 0, 
-        vipPassengersWithEconomySeats = 0, 
-        regularPassengersWithBusinessSeats = 0, 
-        regularPassengersWithEconomySeats = 0;
-        businessSeatsPerFlight = numberFlights * businessSeats
-        economeySeatsPerFlight = numberFlights * economySeats
+        let vipPassengersWithBusinessSeats=0, vipPassengersWithEconomySeats=0, 
+        regularPassengersWithBusinessSeats=0, regularPassengersWithEconomySeats=0;
+        let availableBusinessSeats = nrOfFlights * businessSeatsPerFlight;
+        let availableEconomySeats = nrOfFlights * economySeatsPerFlight;
 
-        if (vip < businessSeatsPerFlight){
-            businessSeatsLeft = businessSeatsPerFlight - vip
-            vipPassengersWithBusinessSeats = vip
-            economySeatsLeft = economeySeatsPerFlight 
-            vip = 0 
-        } else if (vip > businessSeatsPerFlight){
-            vipsLeft = vip - businessSeatsPerFlight
-            vipPassengersWithBusinessSeats = businessSeatsPerFlight
-            businessSeatsLeft = 0
-        } else if (vipsLeft && vipsLeft < economeySeatsPerFlight) {
-            vipPassengersWithEconomySeats = vipsLeft
-            economySeatsLeft = economeySeatsPerFlight - vipsLeft
-        } else if (vipsLeft && vipsLeft > economeySeatsPerFlight) { 
-            throw new Error ("Flight capacity exceeded")
-        } else if (businessSeatsLeft && regular < businessSeatsLeft) {
-            regularPassengersWithBusinessSeats = regular
-            regular = 0
-        } else if (businessSeatsLeft && regular > businessSeatsLeft) {
-            regularPassengersWithBusinessSeats = businessSeatsLeft
-            regularLeft = regular - businessSeatsLeft
-            businessSeatsLeft = 0
-        } else if (economySeatsLeft && regularLeft < economySeatsLeft) {
-            regularPassengersWithEconomySeats = regularLeft
-            regularLeft = 0
-        } else if (economySeatsLeft && regularLeft > economySeatsLeft) {
-            throw new Error ("Flight capacity exceeded")
-        } return {
-            vipWithBusinessSeats: vipPassengersWithBusinessSeats,
-            vipWithEconomySeats: vipPassengersWithEconomySeats,
-            regularWithBusinessSeats: regularPassengersWithBusinessSeats,
-            regualrWithEconomySeats: regularPassengersWithEconomySeats,
-        }
+        var vipBusinessConfiguration = {passengers:vipPassengers, seats:availableBusinessSeats};
+        vipPassengersWithBusinessSeats = updateConfiguration(vipBusinessConfiguration, businessSeatsPerFlight);
+
+        var vipEconomyConfiguration = {passengers:vipBusinessConfiguration.passengers, seats:availableEconomySeats};
+        vipPassengersWithEconomySeats = updateConfiguration(vipEconomyConfiguration, economySeatsPerFlight);
+
+        var regularBusinessConfiguration = {passengers:regularPassengers, seats:vipBusinessConfiguration.seats};
+        regularPassengersWithBusinessSeats = updateConfiguration(regularBusinessConfiguration, businessSeatsPerFlight);
+
+        var regularEconomyConfiguration = {passengers:regularBusinessConfiguration.passengers, seats:vipEconomyConfiguration.seats};
+        regularPassengersWithEconomySeats = updateConfiguration(regularEconomyConfiguration, economySeatsPerFlight);
+
+        return {vipPassengersWithBusinessSeats:vipPassengersWithBusinessSeats,
+                vipPassengersWithEconomySeats:vipPassengersWithEconomySeats, regularPassengersWithBusinessSeats:regularPassengersWithBusinessSeats,
+                regularPassengersWithEconomySeats:regularPassengersWithEconomySeats};
     }
-    // updateConfigoration = (configuration, seatsPerFlight) => {
-    //     let passengersWithSeats = 0
-    //     while(configuration.passenger > 0) {
-    //         if(configuration.seats > 0) {
-    //             if(configuration.passenger >= configuration.seats){
-    //                 if(configuration.seats > configuration.seatsPerFlight){
-    //                     configuration.passenger -+ seatsPerFlight
-    //                 }
-    //             }
-    //         }
-    //     }
 
-    // }
-    return {checkFlightCapacity, distributeAllSeatsToAllPassengers}
+    function updateConfiguration(configuration, seatsPerFlight) {
+        let passengersWithSeats = 0;
+        while (configuration.passengers > 0) {
+            if (configuration.seats > 0) {
+               if (configuration.passengers >= configuration.seats) {
+
+                    if (configuration.seats > configuration.seatsPerFlight) {
+                        configuration.passengers -= seatsPerFlight;
+                        configuration.seats -= seatsPerFlight;
+                        passengersWithSeats += seatsPerFlight;
+                    } else {
+                        configuration.passengers -= configuration.seats;
+                        passengersWithSeats += configuration.seats;
+                        configuration.seats = 0;
+                    }
+               } else {
+                    passengersWithSeats += configuration.passengers;
+                    configuration.seats -= configuration.passengers;
+                    configuration.passengers = 0;
+               }
+            } else {
+               break;
+            }
+        }
+        return passengersWithSeats;
+    }
+
+     return {checkFlightCapacity, distributeAllSeatsToAllPassengers};
 }
 module.exports = Passengers();
