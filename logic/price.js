e38cb1847181eff07f0ab0a57f50a03d89fbcd4f
Peter Lin <plin@petroiq.com>
2021-11-24 09:52:57 -1000

    Completed module

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,13 @@
 function Prices() {
+    calculateFinalPrice = (basePrice, variationPassengerType, variationFlightType) => {
+        var finalPrice = 0
+        passengerPercent = basePrice * (1 + variationPassengerType/100)
+        finalPrice = passengerPercent * (1 + variationFlightType/100)
+        return finalPrice.toFixed(2)
+    }
+
+    return {calculateFinalPrice}
 
 }
 
+module.exports = Prices();
